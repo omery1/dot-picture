a00441655f6b270bd18080e42549128eddc247d7
omery1 <omery1223@gmail.com>
2023-08-16 14:52:22 +0300

    turtle

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
