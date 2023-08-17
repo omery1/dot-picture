9208442a999be95eac108aa209040d8820c3f5c7
omery1 <omery1223@gmail.com>
2023-08-17 13:42:34 +0300

    pendown penup

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ tim = Turtle()
 
 from turtle import Screen
 
+tim.shape("turtle")
+tim.color("red")
+screen=Screen() #This is a class, so we need to create an object of it and then use its methods/attributes (methods
+for _ in range(5):
+    tim.pendown()
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+
+
+
 
 
 
